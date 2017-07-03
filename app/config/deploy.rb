442cc8c054baca254bdca7b3d2311ca6d4231864
camarama <mohamedcra@yahoo.fr>
2017-07-03 15:40:46 +0200

    deployement capifony

---
+++ app/config/deploy.rb
@@ -0,0 +1,20 @@
+set :application, "med"
+set :domain,      "camaramohamed.fr"
+set :deploy_to,   "/var/www/camaramohamed.fr"
+set :app_path,    "app"
+
+set :repository,  "git@github.com:camarama/med.git"
+set :scm,         :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+
+set :model_manager, "doctrine"
+# Or: `propel`
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain, :primary => true       # This may be the same as your `Web` server
+
+set  :use_sudo, false
+set  :keep_releases,  3
+
+# Be more verbose by uncommenting the following line
+logger.level = Logger::MAX_LEVEL
